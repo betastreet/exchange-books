cd757f45c8e405ce531ca3164da1a508549edce2
Paul Leduc <leduc.paul@gmail.com>
2016-11-04 07:46:48 -0700

    cleaned up the server listening and app index files

--- app/index.js
+++ app/index.js
@@ -1,22 +1,10 @@
-// Config
 require('dotenv').config();
-
-// Node Modules
+require('./lib/database');
+require('./lib/cache');
 const restify = require('restify');
-const debug = require('debug');
-const bookshelf = require('./lib/database');
-const redis = require('./lib/cache');
-
-// Server
-const server = require('./lib/http')({
-	name: process.env.APP_NAME,
-	version: '1.0.0',
-});
+const server = require('./lib/server')();
+require('./lib/routes')(server);
 
 // Application wide middleware
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
-
-const routes = require('./lib/routes')(server);
-
-server.listen(process.env.PORT);
