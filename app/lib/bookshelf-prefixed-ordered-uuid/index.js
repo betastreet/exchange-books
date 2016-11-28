1d62fe41df7a702363c18c3c55e7fa5b2802894c
Paul Leduc <leduc.paul@gmail.com>
2016-11-28 14:01:19 -0800

    added modelbase bookshelf plugin

---
+++ app/lib/bookshelf-prefixed-ordered-uuid/index.js
@@ -0,0 +1,117 @@
+const OrderedUUID = require('ordered-uuid');
+
+/**
+ * A function that can be used as a plugin for bookshelf
+ * @param {Object} bookshelf The main bookshelf instance
+ */
+module.exports = (bookshelf) => {
+    const modelPrototype = bookshelf.Model.prototype;
+
+    // "Static" methods
+    bookshelf.Model.generateUuid = function (orderedUuidPrefix) {
+        return (orderedUuidPrefix || '') + OrderedUUID.generate();
+    };
+
+    bookshelf.Model.prefixedUuidToBinary = function (uuid, orderedUuidPrefixLength) {
+        if (orderedUuidPrefixLength) {
+            const prefix = uuid.substring(0, orderedUuidPrefixLength || 2);
+            const uid = uuid.substring(orderedUuidPrefixLength || 2);
+            return Buffer.concat([new Buffer(prefix.toString('binary'), 'binary'), new Buffer(uid, 'hex')]);
+        }
+        return new Buffer(uuid, 'hex');
+    };
+
+    bookshelf.Model.binaryToPrefixedUuid = function (buff, orderedUuidPrefixLength) {
+        if (orderedUuidPrefixLength) {
+            const prefix = buff.slice(0, orderedUuidPrefixLength || 2);
+            const uid = buff.slice(orderedUuidPrefixLength || 2);
+            return prefix.toString() + uid.toString('hex');
+        }
+        return buff.toString('hex');
+    };
+
+    bookshelf.Model.prefixedUuidRegex = function (orderedUuidPrefix) {
+        return new RegExp('/^' + (orderedUuidPrefix || '') + '[a-z0-9]{32}$/');
+    };
+
+    // Extends the default model class
+    bookshelf.Model = bookshelf.Model.extend({
+        initialize: function () {
+            modelPrototype.initialize.call(this);
+
+            if (this.orderedUuids && Array.isArray(this.orderedUuids)) {
+                this.on('saving', this.writeDefaults);
+                this.on('fetching', this.writeDefaults);
+                this.on('destroying', this.writeDefaults);
+                this.on('saved', this.readDefaults);
+                this.on('fetched', this.readDefaults);
+                this.on('destroyed', this.readDefaults);
+                this.on('fetched:collection', this.readCollectionDefaults);
+                this.on('fetching:collection', this.writeCollectionDefaults);
+            }
+        },
+
+        writeDefaults: function (model, columns, options) {
+            this.orderedUuids.forEach((column) => {
+                if (!this.attributes[column] && column === this.idAttribute) this.set(column, bookshelf.Model.generateUuid(this.orderedUuidPrefix));
+                if (this.attributes[column]) this.set(column, bookshelf.Model.prefixedUuidToBinary(this.attributes[column], (this.orderedUuidPrefix ? this.orderedUuidPrefix.length : null)));
+            });
+            // hackey work-around to modify the knex statement's where clauses to the applicable converted valuess
+            if (this.orderedUuids
+                && options
+                && options.query
+                && options.query._statements
+                && options.query._statements.length) {
+                options.query._statements = options.query._statements.map(function (stmt) {
+                    if (!Buffer.isBuffer(stmt.value)) {
+                        this.orderedUuids.forEach((column) => {
+                            if ((stmt.column === `${this.tableName}.${column}` || stmt.column === column) && stmt.value) {
+                                stmt.value = bookshelf.Model.prefixedUuidToBinary(stmt.value,
+                                    (this.orderedUuidPrefix ? this.orderedUuidPrefix.length : null));
+                            }
+                        });
+                    }
+                    return stmt;
+                }, this);
+            }
+            return this;
+        },
+
+        readDefaults: function () {
+            this.orderedUuids.forEach((column) => {
+                if (this.attributes[column]) this.set(column, bookshelf.Model.binaryToPrefixedUuid(this.attributes[column], (this.orderedUuidPrefix ? this.orderedUuidPrefix.length : null)));
+            });
+        },
+
+        readCollectionDefaults: function (collection) {
+            collection.each(function (model) {
+                model.readDefaults();
+            });
+        },
+
+        writeCollectionDefaults: function (collection, columns, options) {
+            // hackey work-around to modify the knex statement's where clauses to the applicable converted valuess
+            if (this.orderedUuids
+                && options
+                && options.query
+                && options.query._statements
+                && options.query._statements.length) {
+                options.query._statements = options.query._statements.map(function (stmt) {
+                    if (!Buffer.isBuffer(stmt.value)) {
+                        this.orderedUuids.forEach((column) => {
+                            if ((stmt.column === `${this.tableName}.${column}` || stmt.column === column) && stmt.value) {
+                                stmt.value = bookshelf.Model.prefixedUuidToBinary(stmt.value,
+                                    (this.orderedUuidPrefix ? this.orderedUuidPrefix.length : null));
+                            }
+                        });
+                    }
+                    return stmt;
+                }, this);
+            }
+        },
+
+        prefixedUuidRegex: function () {
+            return new RegExp('/^[A-Z]{' + (this.orderedUuidPrefix ? this.orderedUuidPrefix.length : 0) + '}[a-z0-9]{32}$/');
+        },
+    });
+};
--- app/lib/models/book.js
+++ app/lib/models/book.js
@@ -1,8 +1,15 @@
 const bookshelf = require('database');
+const ModelBase = require('bookshelf-modelbase')(bookshelf);
+var Joi = require('joi');
 
-module.exports = bookshelf.Model.extend({
+module.exports = ModelBase.extend({
     tableName: 'books',
     orderedUuids: ['id', 'author_id'],
     orderedUuidPrefix: 'BO',
     softDelete: true,
+    validate: {
+        id: Joi.string().regex(bookshelf.Model.prefixedUuidRegex('BO')),
+        author_id: Joi.string().regex(bookshelf.Model.prefixedUuidRegex('AU')).required(),
+        title: Joi.string().min(1).max(100).required(),
+    },
 });
